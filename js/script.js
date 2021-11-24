cf44a3679fe0229c22c31036e084909e66fda02d
Kim dongwon <skqorwkdal@naver.com>
2021-11-24 12:23:14 +0900

    3 commit

--- js/script.js
+++ js/script.js
@@ -21,3 +21,43 @@ themeToggler.onclick = () => {
     document.body.classList.remove('active')
   }
 }
+
+let target = document.querySelector('#dynamic')
+
+function randomString() {
+  let stringArr = [
+    '생각하며 , 행동하는 !',
+    '코딩을 즐길줄 아는 ',
+    '에러와 친한',
+    '배움을 두려워 하지 않는 !',
+  ]
+  let selectString = stringArr[Math.floor(Math.random() * stringArr.length)]
+  let selectStringArr = selectString.split('')
+
+  return selectStringArr
+}
+
+//타이핑리셋
+function resetTyping() {
+  target.textContent = ''
+  dynamic(randomString())
+}
+
+//한글자씩 텍스트 출력 함수
+function dynamic(randomArr) {
+  if (randomArr.length > 0) {
+    target.textContent += randomArr.shift()
+    setTimeout(function () {
+      dynamic(randomArr)
+    }, 80)
+  } else {
+    setTimeout(resetTyping, 3000)
+  }
+}
+dynamic(randomString())
+
+//커서 깜빡임 효과
+function blink() {
+  target.classList.toggle('active')
+}
+setInterval(blink, 500)
